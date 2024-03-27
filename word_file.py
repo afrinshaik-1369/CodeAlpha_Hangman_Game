b8d292a2c73696e210a6da9ed39685141dea42da
afrinshaik-1369 <164766687+afrinshaik-1369@users.noreply.github.com>
2024-03-27 19:17:44 +0530

    Add files via upload

---
+++ word_file.py
@@ -0,0 +1,6 @@
+words=[
+    "Apple","Banana","Orange","Mango","Strawberry","Grape","Pineapple","Watermelon","Kiwi","Peach",
+    "Plum","Cherry","Raspberry","Blueberry","Papaya","Avocado","Pomegranate","Lemon","Lime","Grapes",
+    "Cantaloupe","Fig","Persimmon","Apricot","Nectarine","Coconut","Blackberry","Cranberry","Date","Guava",
+    "Pear","Lychee","Tangerine","Olive","Elderberry","Boysenberry","Huckleberry","Gooseberry","Dragon fruit"
+     ]
